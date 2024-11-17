df0027ce2a17a3098378a37014bd72300a4bd898
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-11-17 17:59:23 +0200

    plugins in begin

---
+++ wp-content/plugins/softuni-practice/softuni-practice.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: SoftUni
+ * Author: Nasko
+ * Description: My first plugin!
+ */
+
+// echo "Hello, Nasko, from my first plugin!";
+
+var_dump( "Hello, Nasko, from my first plugin!" );
+// die();
