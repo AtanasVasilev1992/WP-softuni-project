cda2834f50f24c281b111d2c5fffc7a036f923f9
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-11-22 14:35:22 +0200

    Add single.php and show dinamic content

--- wp-content/themes/softuni-fruits/functions.php
+++ wp-content/themes/softuni-fruits/functions.php
@@ -1,5 +1,8 @@
 <?php 
 
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
+
 add_action( 'wp_enqueue_scripts' , 'softuni_enqueue_assets' );
 function softuni_enqueue_assets() {
     wp_enqueue_style( 'softuni' , get_stylesheet_directory_uri() . './style.css' );
