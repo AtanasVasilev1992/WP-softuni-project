1a0f74a9c7302a24909fdee8bbb1ae259bc7be36
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-11-23 16:42:22 +0200

    Rework single.php

--- wp-content/themes/softuni-fruits/single.php
+++ wp-content/themes/softuni-fruits/single.php
@@ -1,50 +1,50 @@
 <?php get_header(); ?>
-    
 
-    <!-- <?php if ( have_posts() ) : ?> -->
+
+<!-- <?php if (have_posts()) : ?> -->
 <!-- breadcrumb-section -->
 <div class="breadcrumb-section breadcrumb-bg">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-8 offset-lg-2 text-center">
-					<div class="breadcrumb-text">
-						<p>See more Details</p>
-						<h1><?php echo get_the_title(); ?></h1>
-					</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 offset-lg-2 text-center">
+				<div class="breadcrumb-text">
+					<p>See more Details</p>
+					<h1><?php echo get_the_title(); ?></h1>
 				</div>
 			</div>
 		</div>
 	</div>
-	<!-- end breadcrumb section -->
+</div>
+<!-- end breadcrumb section -->
 
-	<!-- single product -->
-	<div class="single-product mt-150 mb-150">
-		<div class="container">
-			<div class="row">
-                
-                <?php if ( has_post_thumbnail() ) : ?>
+<!-- single product -->
+<div class="single-product mt-150 mb-150">
+	<div class="container">
+		<div class="row">
+
+			<?php if (has_post_thumbnail()) : ?>
 				<div class="col-md-5">
 					<div class="single-product-img">
-                        <?php the_post_thumbnail('post-thumbnail', ['class' => '', 'title' => 'Feature image']); ?>
+						<?php the_post_thumbnail('post-thumbnail', ['class' => '', 'title' => 'Feature image']); ?>
 					</div>
 				</div>
-                <?php endif; ?>
+			<?php endif; ?>
 
-				<div class="col-md-7">
-					<div class="single-product-content">
-						<h3><?php echo get_the_title(); ?></h3>
-						<?php echo the_content(); ?>
-							<p><strong>Categories: </strong>Fruits, Organic</p>
-						</div>
-					</div>
+			<div class="col-md-7">
+				<div class="single-product-content">
+					<h3><?php echo get_the_title(); ?></h3>
+					<?php echo the_content(); ?>
+					<p><strong>Categories: </strong>Fruits, Organic</p>
 				</div>
 			</div>
 		</div>
 	</div>
-	<!-- end single product -->
+</div>
+</div>
+<!-- end single product -->
 
-    
-    <!-- <?php endif; ?> -->
 
+<!-- <?php endif; ?> -->
 
+<?php fruits_display_latest_posts(); ?>
 <?php get_footer(); ?>
