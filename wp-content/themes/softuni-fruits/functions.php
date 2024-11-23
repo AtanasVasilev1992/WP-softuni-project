7348856c91532f712e0a24aeecafe4b6af36ea6d
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-11-23 16:40:21 +0200

    Rework function

--- wp-content/themes/softuni-fruits/functions.php
+++ wp-content/themes/softuni-fruits/functions.php
@@ -1,9 +1,15 @@
-<?php 
+<?php
 
 add_theme_support( 'title-tag' );
 add_theme_support( 'post-thumbnails' );
+add_post_type_support( 'excerpt' , array() );
 
 add_action( 'wp_enqueue_scripts' , 'softuni_enqueue_assets' );
-function softuni_enqueue_assets() {
-    wp_enqueue_style( 'softuni' , get_stylesheet_directory_uri() . './style.css' );
-}
+function softuni_enqueue_assets()
+{
+    wp_enqueue_style( 'softuni' , get_stylesheet_directory_uri() . './style.css');
+};
+
+function fruits_display_latest_posts( $number_of_posts = 3 ) {
+    include 'latest-posts.php';
+};
