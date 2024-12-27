9ac0068af56f3b29edb0e054d3421b6ac807b686
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-12-27 18:23:16 +0200

    Optimaze Latest posts, show dynamic year

--- wp-content/themes/softuni-fruits/latest-posts.php
+++ wp-content/themes/softuni-fruits/latest-posts.php
@@ -3,13 +3,15 @@ $latest_posts_args = array(
     'post_type'      => 'post',
     'post_status'    => 'publish',
     'posts_per_page' => $number_of_posts,
+    'orderby'        => 'date',
+    'order'          => 'DESC',
 );
 
 $latest_posts_query = new WP_Query( $latest_posts_args );
 ?>
 
-<?php if ( $latest_posts_query -> have_posts() ) : ?>
-    <!-- latest news -->
+<?php if ( $latest_posts_query->have_posts() ) : ?>
+    
     <div class="latest-news pt-150 pb-150">
         <div class="container">
 
@@ -24,20 +26,35 @@ $latest_posts_query = new WP_Query( $latest_posts_args );
 
             <div class="row">
 
-            <?php while( $latest_posts_query -> have_posts() ) : $latest_posts_query -> the_post() ; ?>
+            <?php while( $latest_posts_query->have_posts() ) : $latest_posts_query->the_post() ; ?>
                 <div class="col-lg-4 col-md-6">
                     <div class="single-product-item">
-                        <a href="<?php echo get_the_permalink(); ?>">
-                        <?php the_post_thumbnail('post-thumbnail', ['class' => '', 'title' => 'Feature image']); ?>
-                        </a>
+
+                        <?php if ( has_post_thumbnail() ) : ?>
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_post_thumbnail( 'post-thumbnail', [ 'class' => 'post-thumb', 'title' => get_the_title() ] ); ?>
+                            </a>
+                        <?php endif; ?>
+
                         <div class="news-text-box">
-                            <h3 class="single-product-href"><a href="<?php echo get_the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                            <h3 class="single-product-href">
+                                <a href="<?php echo get_the_permalink(); ?>"><?php the_title(); ?></a>
+                            </h3>
+
                             <p class="blog-meta">
-                                <span class="author"><i class="fas fa-user"></i> <?php echo get_the_author_meta( 'nickname' ); ?></span>
-                                <span class="date"><i class="fas fa-calendar"></i> <?php echo get_the_date(); ?></span>
+                                <span class="author">
+                                    <i class="fas fa-user"></i> <?php the_author(); ?>
+                                </span>
+                                <span class="date">
+                                    <i class="fas fa-calendar"></i> <?php echo get_the_date(); ?>
+                                </span>
                             </p>
+                            
                             <p class="excerpt"><?php the_excerpt(); ?></p>
-                            <a href="<?php echo get_the_permalink(); ?>" class="boxed-btn">read more <i class="fas fa-angle-right"></i></a>
+                            <a href="<?php the_permalink(); ?>" class="boxed-btn">
+                                <?php esc_html_e( 'Read More', 'softuni' ); ?> 
+                                <i class="fas fa-angle-right"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -46,12 +63,17 @@ $latest_posts_query = new WP_Query( $latest_posts_args );
             </div>
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <a href="<?php echo site_url( '/2024/' ); ?>" class="boxed-btn">More News</a>
+                <?php 
+                    $current_year = date('Y');
+                    $archive_link = get_year_link($current_year);
+                    ?>
+                    <a href="<?php echo esc_url($archive_link); ?>" class="boxed-btn">
+                        <?php esc_html_e('More News', 'softuni'); ?>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
-    <!-- end latest news -->
 <?php endif; ?>
 
 <?php wp_reset_postdata(); ?>
